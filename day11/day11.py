0dbcdfb715a8b930945b6087efa745d088097c5b
Robin Hagmayer <robin.hagmayer@gmail.com>
2021-12-11 08:51:08 +0100

    Implemented solution for part 2

--- day11/day11.py
+++ day11/day11.py
@@ -116,7 +116,110 @@ def solve1(inp):
 
 
 def solve2(inp):
-    pass
+    k = 0
+    while True:
+        k += 1
+        flash_pos = []
+
+        for i, line in enumerate(inp):
+            for j, num in enumerate(line):
+                inp[i][j] += 1
+
+                if num + 1 > 9:
+                    flash_pos.append((i, j))
+        
+        already_flashed = []
+        
+        while flash_pos:
+            i, j = flash_pos.pop(0)
+            already_flashed.append((i,j))
+
+            if i == 0:
+                pass
+            elif j == 0:
+                inp[i-1][j] += 1
+                if inp[i-1][j] > 9 and (i-1,j) not in already_flashed and (i-1,j) not in flash_pos:
+                    flash_pos.append((i-1, j))
+
+                inp[i-1][j+1] += 1
+                if inp[i-1][j+1] > 9 and (i-1,j+1) not in already_flashed and (i-1,j+1) not in flash_pos:
+                    flash_pos.append((i-1, j+1))
+            elif j == len(inp[i])-1:
+                inp[i-1][j] += 1
+                if inp[i-1][j] > 9 and (i-1, j) not in already_flashed and (i-1, j) not in flash_pos:
+                    flash_pos.append((i-1, j))
+
+                inp[i-1][j-1] += 1
+                if inp[i-1][j-1] > 9 and (i-1,j-1) not in already_flashed and (i-1,j-1) not in flash_pos:
+                    flash_pos.append((i-1, j-1))
+            else:
+                inp[i-1][j] += 1
+                if inp[i-1][j] > 9 and (i-1, j) not in already_flashed and (i-1, j) not in flash_pos:
+                    flash_pos.append((i-1, j))
+
+                inp[i-1][j-1] += 1
+                if inp[i-1][j-1] > 9 and (i-1,j-1) not in already_flashed and (i-1,j-1) not in flash_pos:
+                    flash_pos.append((i-1, j-1))
+
+                inp[i-1][j+1] += 1
+                if inp[i-1][j+1] > 9 and (i-1,j+1) not in already_flashed and (i-1,j+1) not in flash_pos:
+                    flash_pos.append((i-1, j+1))
+
+            if i == len(inp)-1:
+                pass
+            elif j == 0:
+                inp[i+1][j] += 1
+                if inp[i+1][j] > 9 and (i+1,j) not in already_flashed and (i+1,j) not in flash_pos:
+                    flash_pos.append((i+1, j))
+
+                inp[i+1][j+1] += 1
+                if inp[i+1][j+1] > 9 and (i+1,j+1) not in already_flashed and (i+1,j+1) not in flash_pos:
+                    flash_pos.append((i+1, j+1))
+            elif j == len(inp[i])-1:
+                inp[i+1][j] += 1
+                if inp[i+1][j] > 9 and (i+1,j) not in already_flashed and (i+1,j) not in flash_pos:
+                    flash_pos.append((i+1, j))
+
+                inp[i+1][j-1] += 1
+                if inp[i+1][j-1] > 9 and (i+1,j-1) not in already_flashed and (i+1,j-1) not in flash_pos:
+                    flash_pos.append((i+1, j-1))
+            else:
+                inp[i+1][j] += 1
+                if inp[i+1][j] > 9 and (i+1,j) not in already_flashed and (i+1,j) not in flash_pos:
+                    flash_pos.append((i+1, j))
+
+                inp[i+1][j-1] += 1
+                if inp[i+1][j-1] > 9 and (i+1,j-1) not in already_flashed and (i+1,j-1) not in flash_pos:
+                    flash_pos.append((i+1, j-1))
+
+                inp[i+1][j+1] += 1
+                if inp[i+1][j+1] > 9 and (i+1,j+1) not in already_flashed and (i+1,j+1) not in flash_pos:
+                    flash_pos.append((i+1, j+1))
+            
+            if j == 0:
+                inp[i][j+1] += 1
+                if inp[i][j+1] > 9 and (i,j+1) not in already_flashed and (i,j+1) not in flash_pos:
+                    flash_pos.append((i, j+1))
+            elif j == len(inp[i])-1:
+                inp[i][j-1] += 1
+                if inp[i][j-1] > 9 and (i,j-1) not in already_flashed and (i,j-1) not in flash_pos:
+                    flash_pos.append((i, j-1))
+            else:
+                inp[i][j-1] += 1
+                if inp[i][j-1] > 9 and (i,j-1) not in already_flashed and (i,j-1) not in flash_pos:
+                    flash_pos.append((i, j-1))
+
+                inp[i][j+1] += 1
+                if inp[i][j+1] > 9 and (i,j+1) not in already_flashed and (i,j+1) not in flash_pos:
+                    flash_pos.append((i, j+1))
+            
+        for i, j in already_flashed:
+            inp[i][j] = 0
+        
+        if len(already_flashed) == 100:
+            break
+    
+    return k
 
 
 path = "day11/day11_input.txt"
