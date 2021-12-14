5da6b7e5dac8cb7b2b62c1f4dcc09e1fbb7b30d6
Robin Hagmayer <robin.hagmayer@gmail.com>
2021-12-14 11:36:04 +0100

    Implemented solution for part 1

--- day14/day14.py
+++ day14/day14.py
@@ -3,22 +3,49 @@
 
 
 from time import perf_counter as pfc
+from collections import Counter
 
 
 def read(path: str):
     with open(path) as my_file:
-        return my_file.read().rstrip()
+        both = my_file.read().rstrip().split('\n\n')
+        template = [element for element in both[0].rstrip()]
+        rules = {rule.split(' -> ')[0]: rule.split(' -> ')[1] for rule in both[1].splitlines()}
+        
+        return template, rules
 
 
 def solve1(inp):
-    pass
+    template = inp[0]
+    rules = inp[1]
+
+    for _ in range(10):
+        new_template = []
+
+        for i, element in enumerate(template):
+            if i+1 >= len(template):
+                break
+
+            pair = element + template[i+1]
+            rule = rules[pair]
+
+            if i == 0:
+                new_template.append(element)
+            new_template.append(rule)
+            new_template.append(template[i+1])
+        
+        template = new_template
+    
+    count = Counter(template).values()
+
+    return max(count) - min(count)
 
 
 def solve2(inp):
     pass
 
 
-path = "day14/day14_sample.txt"
+path = "day14/day14_input.txt"
 inp1 = read(path)
 inp2 = read(path)
 
