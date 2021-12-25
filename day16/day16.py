66f4516644a0e3c6ac875f41650f4722466b3f33
Robin Hagmayer <robin.hagmayer@gmail.com>
2021-12-25 13:41:10 +0100

    Finished day 16

--- day16/day16.py
+++ day16/day16.py
@@ -3,16 +3,17 @@
 
 
 from time import perf_counter as pfc
+from functools import reduce
 
 
 def read(path: str):
     with open(path) as my_file:
         hex_as_string = my_file.read().rstrip()
         binary = ''
-        
+
         for char in hex_as_string:
             binary += format(int(char, 16), '04b')
-        
+
         return binary
 
 
@@ -30,69 +31,181 @@ def match_literal(literal: str):
             case '0':
                 information += current_group[1:5]
                 break
-    
+
     return literal_length*5, information
 
 
-def match_operator(operator: str):
-    subpacket_total_length = 0
-
-    match operator[0]:
-        case '0':
-            subpacket_total_length = int(operator[1:16], 2)
-            current_subpacket_length = subpacket_total_length
-            current_packet = operator[16:]
-
-            while current_subpacket_length > 0:
-                current_packet, current_subpacket_length = match_packet(current_packet, 0, current_subpacket_length)
-            
-            return operator[16+subpacket_total_length:]
-
-        case '1':
-            subpacket_number = int(operator[1:12], 2)
-            current_packet = operator[12:]
-            
-            for _ in range(subpacket_number):
-                current_packet = match_packet(current_packet, 1)
-            
-            return current_packet
-
-
-def match_packet(packet: str, id: int=-1, subpacket_length: int=0):
-    versions.append(int(packet[:3], 2))
-    literal_length = -1
-
-    match packet[3:6]:
-        case '100':
-            literal_length, _ = match_literal(packet[6:])
-        case _:
-            new_packet = match_operator(packet[6:])
-    
-    match id:
-        case 0:
-            if literal_length == -1:
-                return new_packet, subpacket_length-(len(packet)-len(new_packet))
-            subpacket_length -= 6+literal_length
-            return packet[6+literal_length:6+literal_length+subpacket_length], subpacket_length
-        case 1:
-            if literal_length == -1:
-                return new_packet
-            return packet[6+literal_length:]
-        case -1:
-            pass
-
-
-def solve1(inp):
+def parse(start: str, packet_num: int = 0):
     global versions
-    versions = []
+    versions += int(data[start:start+3], 2)
+    curr_lit = []
 
-    match_packet(inp)
+    match data[start+3:start+6]:
+        case '000':
+            if data[start+6] == '0':
+                end = start+22+int(data[start+7:start+22], 2)
+                curr_start = start+22
 
-    return sum(versions)
+                while curr_start < end:
+                    curr_start, res = parse(curr_start)
+                    curr_lit.append(res)
+
+                return curr_start, sum(curr_lit)
+            else:
+                packet_num = int(data[start+7:start+18], 2)
+                curr_start = start+18
+
+                while packet_num > 0:
+                    curr_start, res = parse(curr_start, packet_num)
+                    curr_lit.append(res)
+                    packet_num -= 1
+
+                return curr_start, sum(curr_lit)
+        case '001':
+            if data[start+6] == '0':
+                end = start+22+int(data[start+7:start+22], 2)
+                curr_start = start+22
+
+                while curr_start < end:
+                    curr_start, res = parse(curr_start)
+                    curr_lit.append(res)
+
+                return curr_start, reduce((lambda x, y: x * y), curr_lit)
+            else:
+                packet_num = int(data[start+7:start+18], 2)
+                curr_start = start+18
+
+                while packet_num > 0:
+                    curr_start, res = parse(curr_start, packet_num)
+                    curr_lit.append(res)
+                    packet_num -= 1
+
+                return curr_start, reduce((lambda x, y: x * y), curr_lit)
+        case '010':
+            if data[start+6] == '0':
+                end = start+22+int(data[start+7:start+22], 2)
+                curr_start = start+22
+
+                while curr_start < end:
+                    curr_start, res = parse(curr_start)
+                    curr_lit.append(res)
+
+                return curr_start, min(curr_lit)
+            else:
+                packet_num = int(data[start+7:start+18], 2)
+                curr_start = start+18
+
+                while packet_num > 0:
+                    curr_start, res = parse(curr_start, packet_num)
+                    curr_lit.append(res)
+                    packet_num -= 1
+
+                return curr_start, min(curr_lit)
+        case '011':
+            if data[start+6] == '0':
+                end = start+22+int(data[start+7:start+22], 2)
+                curr_start = start+22
+
+                while curr_start < end:
+                    curr_start, res = parse(curr_start)
+                    curr_lit.append(res)
+
+                return curr_start, max(curr_lit)
+            else:
+                packet_num = int(data[start+7:start+18], 2)
+                curr_start = start+18
+
+                while packet_num > 0:
+                    curr_start, res = parse(curr_start, packet_num)
+                    curr_lit.append(res)
+                    packet_num -= 1
+
+                return curr_start, max(curr_lit)
+        case '100':
+            lit_len, literal = match_literal(data[start+6:])
+            return start+6+lit_len, int(literal, 2)
+        case '101':
+            if data[start+6] == '0':
+                end = start+22+int(data[start+7:start+22], 2)
+                curr_start = start+22
+
+                while curr_start < end:
+                    curr_start, res = parse(curr_start)
+                    curr_lit.append(res)
+
+                return curr_start, 1 if curr_lit[0] > curr_lit[1] else 0
+            else:
+                packet_num = int(data[start+7:start+18], 2)
+                curr_start = start+18
+
+                while packet_num > 0:
+                    curr_start, res = parse(curr_start, packet_num)
+                    curr_lit.append(res)
+                    packet_num -= 1
+
+                return curr_start, 1 if curr_lit[0] > curr_lit[1] else 0
+        case '110':
+            if data[start+6] == '0':
+                end = start+22+int(data[start+7:start+22], 2)
+                curr_start = start+22
+
+                while curr_start < end:
+                    curr_start, res = parse(curr_start)
+                    curr_lit.append(res)
+
+                return curr_start, 1 if curr_lit[0] < curr_lit[1] else 0
+            else:
+                packet_num = int(data[start+7:start+18], 2)
+                curr_start = start+18
+
+                while packet_num > 0:
+                    curr_start, res = parse(curr_start, packet_num)
+                    curr_lit.append(res)
+                    packet_num -= 1
+
+                return curr_start, 1 if curr_lit[0] < curr_lit[1] else 0
+        case '111':
+            if data[start+6] == '0':
+                end = start+22+int(data[start+7:start+22], 2)
+                curr_start = start+22
+
+                while curr_start < end:
+                    curr_start, res = parse(curr_start)
+                    curr_lit.append(res)
+
+                return curr_start, 1 if curr_lit[0] == curr_lit[1] else 0
+            else:
+                packet_num = int(data[start+7:start+18], 2)
+                curr_start = start+18
+
+                while packet_num > 0:
+                    curr_start, res = parse(curr_start, packet_num)
+                    curr_lit.append(res)
+                    packet_num -= 1
+
+                return curr_start, 1 if curr_lit[0] == curr_lit[1] else 0
+
+
+def solve1(inp: str):
+    global versions
+    global data
+    versions = 0
+    data = inp
+
+    _, _ = parse(0)
+
+    return versions
 
 
 def solve2(inp):
-    pass
+    global versions
+    global data
+    versions = 0
+    data = inp
+
+    _, res = parse(0)
+
+    return res
 
 
 path = "day16/day16_input.txt"
